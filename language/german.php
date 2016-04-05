c81610178a3cda0da01d7dd3ca25a50612991547
Bloodsoul <bloodsoul@dragon-force.org>
2016-04-05 11:17:21 +0200

    initial game class and language

---
+++ language/german.php
@@ -0,0 +1,66 @@
+<?php
+/*	Project:	EQdkp-Plus
+ *	Package:	Blade & Soul game package
+ *	Link:		http://eqdkp-plus.eu
+ *
+ *	Copyright (C) 2006-2016 EQdkp-Plus Developer Team
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Affero General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Affero General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+					
+$german_array = array (
+	'factions' => array(
+    		'cerulean_order' => 'Der Coelinorden',
+    		'crimson_legion' => 'Die Karminlegion',
+  	),
+  	'races' => array( 
+		0 => 'Unbekannt',
+    		1 => 'Gon',
+    		2 => 'Lyn',
+    		3 => 'Yun',
+    		4 => 'Jin',
+  	),
+  	'classes' => array( 
+    		0 => 'Unbekannt',
+    		1 => 'Klingenmeister',
+    		2 => 'Berserker',
+   		3 => 'Beschwörer',
+    		4 => 'Gewaltenbändiger',
+    		5 => 'Kungfu-Meister',
+    		6 => 'Nachtklinge',
+    		7 => 'Klingentänzer',
+    		8 => 'Paktierer',
+  	),
+	'genders' => array(
+		0 => 'Männlich',
+		1 => 'Weiblich',
+	),
+  	'lang' => array(
+		'bs'				=> 'Blade & Soul',
+		'core_sett_fs_gamesettings'	=> 'Blade & Soul Settings',
+		'uc_faction'			=> 'Fraktion',
+		'uc_gender'			=> 'Geschlecht',
+		'uc_guild'			=> 'Gilde',
+		'uc_race'			=> 'Volk',
+		'uc_class'			=> 'Klasse',
+		'uc_level'			=> 'Level',
+		'uc_hongmoon'			=> 'Hongmoon Level',
+  	),
+);
+		
+?>
